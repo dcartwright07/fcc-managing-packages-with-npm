db1cd7498ebc1d8bcb90b0154cb3b12a4a65ba00
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-17 15:59:26 -0400

    Update router file with styles

--- myApp.js
+++ myApp.js
@@ -1,43 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
  module.exports = app;
