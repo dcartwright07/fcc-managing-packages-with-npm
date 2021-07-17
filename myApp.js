f19cab6a679c916524853592c13bf6b110f1f845
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-17 14:13:02 -0400

    Complete start with a working express server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-console.log("Hello World");
+function getString(req, res) {
+	res.send("Hello Express");
+}
+
+app.get("/", getString);
 
 
 
