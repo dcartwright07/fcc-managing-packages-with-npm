50e2cc0470eb9ce35e817bb3fe49acea2ff3c843
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-07-17 14:27:21 -0400

    Refactor code to simplify

--- myApp.js
+++ myApp.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var app = express();
 
-function getString(req, res) {
+app.get("/", function(req, res) {
 	res.send("Hello Express");
-}
-
-app.get("/", getString);
+});
 
 
 
